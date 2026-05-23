c454644ff6f710beb00b1372078a7252cc85390b
HussainAli7858 <hussain7858alisabir@gmail.com>
2026-05-23 13:14:30 +0500

    create signup api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'// Import userRouter from the user.route.js file but i export the routerin the file but in the index.js file i am importing it as userRouter, i can change the name of import because it is a default export,in case of default export we can import it with any name we want, but in case of named export we have to import it with the same name as it is exported. 
 
+import authRouter from './routes/auth.route.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,8 +14,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const PORT = 3000;
 const app = express()
+// Middleware to parse JSON bodies
+app.use(express.json())
 
 app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
