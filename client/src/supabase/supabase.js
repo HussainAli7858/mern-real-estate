030dba17d482d0d6e4b9e4e45961058e81a27711
HussainAli7858 <hussain7858alisabir@gmail.com>
2026-06-04 10:14:39 +0500

    simple chage in supabse storage

--- client/src/supabase/supabase.js
+++ client/src/supabase/supabase.js
@@ -2,9 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-
-console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("KEY EXISTS:", !!import.meta.env.VITE_SUPABASE_KEY);
+ 
 export const supabase = createClient(
   supabaseUrl,
   supabaseKey
